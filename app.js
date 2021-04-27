791e7be9be1dd8462ff41fe6802f9fa8182c4474
Vivek Kumar Shrivastava <shrivastava.96vivek@gmail.com>
2021-04-27 10:56:50 +0530

    update: Backend completed

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var output_text = document.querySelector("#output");
+var ourURL = "https://api.funtranslations.com/translate/minion.json";
+
+function translateURL(text) {
+  return (
+    //"https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" +
+    ourURL + "?" + "text=" + text
+  );
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with the server, please try again!");
+}
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
-  output_text.innerText = "asdddand" + txtInput.value;
+  // output_text.innerText = "asdddand" + txtInput.value;
+  fetch(translateURL(txtInput.value))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      output_text.innerText = translatedText;
+    })
+    .catch(errorHandler);
 });
